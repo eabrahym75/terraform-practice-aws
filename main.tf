20384a59532018f32a3fa2b3c3f9a54f20af180d
eabrahym75 <ayantolaibraheemkehinde@gmail.com>
2023-04-24 11:13:14 +0100

    fixed EC2 resource block, commented out Auto-scaling and ALB block

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 # Create EC2 Instance - Ubuntu 20.04 for nginx
-resource "aws_instance" "my-nginx-server" {
-  ami           = ami-0aa2b7722dc1b5612
-  instance_type = t2.micro
-  key_name      = "aws-key"
-	user_data     =  "${file("install_nginx.sh")}"  
-  vpc_security_group_ids = [aws_vpc.my_test_vpc1.id]
+resource "aws_instance" "my-nginx-server2ws" {
+  ami           = "ami-0aa2b7722dc1b5612"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name      = "aws_key"
+	user_data     =  "${file("install-nginx.sh")}"  
+  vpc_security_group_ids = ["${aws_security_group.my_asg.id}"]
   tags = {
     "Name" = "Ubuntu Nginx server"
   }
@@ -12,12 +13,13 @@ resource "aws_instance" "my-nginx-server" {
 
 
 # Create EC2 Instance - Ubuntu 20.04 for apache
-resource "aws_instance" "my-apache-server" {
-  ami           = ami-0aa2b7722dc1b5612
-  instance_type = t2.micro
-  key_name      = "aws-key"
-	user_data     =  "${file("install_apache.sh")}"  
-  vpc_security_group_ids = [aws_vpc.my_test_vpc1.id]
+resource "aws_instance" "my-apache-server1s3" {
+  ami           = "ami-0aa2b7722dc1b5612"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name      = "aws_key"
+	user_data     =  "${file("install-apache.sh")}"  
+  vpc_security_group_ids = ["${aws_security_group.my_asg.id}"]
   tags = {
     "Name" = "Ubuntu Apache server"
   }
