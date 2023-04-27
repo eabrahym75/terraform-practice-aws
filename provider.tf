d0a476c01e611f5f1097208e1329559f66802c7e
eabrahym75 <ayantolaibraheemkehinde@gmail.com>
2023-04-27 12:30:03 +0100

    resolved conflict

--- provider.tf
+++ provider.tf
@@ -8,11 +8,11 @@ terraform {
   backend "s3" {
     bucket = "bucket-ayantola"
     key = "bucket-ayantola/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 #Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  credential = "~/.aws/config"
 }
